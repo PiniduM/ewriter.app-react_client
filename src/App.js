92a8ed0204f9a1a84f15158a5adcc4e93645d09b
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-01-22 12:31:29 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from "./sections/home/HomePage.js"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>home</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/sections/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const HomePage = () => {
+
+
+    return (
+        <>
+            <h1>home</h1>
+            <p>helo</p>
+        </>
+    )
+}
+
+export default HomePage;
