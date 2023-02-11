116896bcb94989b695c40ee9570b85747ab41ba8
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-02-11 06:28:07 +0530

    about to add writer

--- src/sections/essayWriter/EssayWriterRoutes.js
+++ src/sections/essayWriter/EssayWriterRoutes.js
@@ -1,12 +1,15 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+
 const EssayWriterHomePage = lazy(() => import("./pages/EssayWriterHomePage"));
+const Writer = lazy(() => import("./sections/writer/Writer.js"));
 
 const EssayWriterRoutes = () => {
   return (
     <Suspense fallback={<p>loading</p>}>
       <Routes>
         <Route path="/essaywriter" exact element={<EssayWriterHomePage />} />
+        <Route path="/essaywriter_writer" exact element={<Writer />} />
       </Routes>
     </Suspense>
   );
--- src/sections/essayWriter/sections/home/components/Feature.js
+++ src/sections/essayWriter/sections/home/components/Feature.js
@@ -5,8 +5,8 @@ import classes from "./Feature.module.css";
 const Feature = (props) => {
     return (
     <div className={classes.featureContainer}>
-        <div className={classes.icon}>
-            <img src={props.Icon} alt={props.iconAlt} />
+        <div className={classes.iconContainer}>
+            <img src={props.Icon} alt={props.iconAlt} className={classes.icon}/>
         </div>
         <h3 className={classes.h3}>{props.heading}</h3>
         <p className={classes.p}>{props.description}</p>
